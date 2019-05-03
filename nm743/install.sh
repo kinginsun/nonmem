d1e4d358d33c0ca71f1804141ca2f258b87b69c6
Yufeng Zhang <kinginsun@163.com>
2019-05-03 09:42:03 +0800

    up

--- nm743/install.sh
+++ nm743/install.sh
@@ -6,26 +6,35 @@ sed 's/NMHOME/'${p}'/g' execute.dat > execute
 sed 's/NMHOME/'${p}'/g' nmfe74.dat > util/nmfe74
 sudo chmod +x execute
 sudo chmod +x util/nmfe74
-cd /usr/local/bin
-sudo rm execute
-sudo rm nmfe74
-sudo ln -s ${NMRoot}/execute
-sudo ln -s ${NMRoot}/util/nmfe74
-cd ${NMRoot}
+if [ -e '/usr/local/bin' ];then
+  cd /usr/local/bin
+  sudo rm execute
+  sudo rm nmfe74
+  sudo ln -s ${NMRoot}/execute
+  sudo ln -s ${NMRoot}/util/nmfe74
+  cd ${NMRoot}
 
-# pull nonmem image
-#docker pull kinginsun/nonmem:7.4.3
+  cd models
+  echo "===============================TEST ONE==============================="
+  echo "Run test model with execute ......"
+  echo ""
+  echo ""
+  echo ""
+  echo ""
+  echo ""
+  execute CONTROL5.mod
 
-# test nonmem install
-#NMRoot=${PWD}
-#modelFolder=${PWD}
-#d=`date +%s`
-#docker run --rm --name nonmem${d} --workdir /nonmem/nm743/util -v ${NMRoot}/license:/nonmem/nm743/license -v ${modelFolder}/models:/nonmem/models kinginsun/nonmem:7.4.3 /bin/bash nmfe74 CONTROL5 OUTPUT5
+  echo ""
+  echo ""
+  echo ""
+  echo ""
+  echo ""
+  echo "================================TEST TWO==============================="
+  echo "Run test model with nmfe74 ......"
+  nmfe74 CONTROL5.mod OUTPUT5
+  cd ..
+  echo "===============================TEST DONE================================"
 
-cd models
-echo "Run test model with execute ......"
-execute CONTROL5
-
-echo "Run test model with nmfe74 ......"
-nmfe74 CONTROL5 OUTPUT5
-cd ..
+else
+  echo "No /usr/local/bin in your system ..."
+fi
--- nm743/uninstall.sh
+++ nm743/uninstall.sh
@@ -2,10 +2,18 @@
 p=${PWD}
 p=${p//\//\\\/}
 NMRoot=${PWD}
-
-sudo rm execute
+if [ -e 'execute' ];then
+  sudo rm execute
+fi
+if [ -e 'util/nmfe74' ];then
 sudo rm util/nmfe74
+fi
 cd /usr/local/bin
-sudo rm execute
-sudo rm nmfe74
+if [ -e 'execute' ];then
+  sudo rm execute
+fi
+if [ -e 'nmfe74' ];then
+  sudo rm nmfe74
+fi
 cd ${NMRoot}
+echo "Uninstall success!"
